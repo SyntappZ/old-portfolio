2216c8e913b3ffdfcc7a8655cda44e2f69837ffd
biscuitmanz <syntappz@gmail.com>
2018-10-18 20:36:50 +0100

    uploading all files

---
+++ js/main.js
@@ -0,0 +1,26 @@
+anime({
+    targets: '.logo',
+    translateX: {
+        value: 1550,
+        duration: 1200
+      },
+      rotate: {
+        value: 360,
+        delay:1000,
+        duration: 1500,
+        easing: 'easeInOutSine'
+      },
+      scale: {
+        value: 4,
+        duration: 1200,
+        delay: 1500,
+        easing: 'easeInOutQuart'
+      },
+      delay: 1000 
+    });
+    
+    
+
+
+
+   
